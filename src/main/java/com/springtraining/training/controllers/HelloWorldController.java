6fa4364ec865c918dc6e93759f02eeada44a3991
Andrei Morariu <a.morariu@logicainformatica.it>
2025-10-03 22:53:53 +0200

    Hello world

---
+++ src/main/java/com/springtraining/training/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.springtraining.training.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}
